ee1c9cc67efe447db95b934485a7d2b56ec571b6
Ahmad Syarif <syarifahmad817@gmail.com>
2021-11-18 14:47:00 -0800

    add master detail

--- ci_3.sql
+++ ci_3.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Waktu pembuatan: 09 Nov 2021 pada 06.45
--- Versi server: 5.7.26
--- Versi PHP: 7.3.5
+-- Waktu pembuatan: 18 Nov 2021 pada 22.44
+-- Versi server: 5.7.31
+-- Versi PHP: 7.3.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -46,8 +45,37 @@ CREATE TABLE IF NOT EXISTS `pelanggan` (
 INSERT INTO `pelanggan` (`id`, `nama`, `nik`, `hp`, `email`, `alamat`) VALUES
 (1, 'Ahmad Syarif', '1234567890', '08123456789', 'master@mail.com', 'Medan'),
 (3, 'Muhammad', '987654321', '1234567890', 'test@mail.com', 'Medan'),
-(12, 'Ahmad', '1223021511970002', '1234567890', 'test@mail.com', 'Medan'),
 (14, 'AAA', '123456', '0987612345', 'aaa@mail.com', 'Medan');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pesanan`
+--
+
+DROP TABLE IF EXISTS `pesanan`;
+CREATE TABLE IF NOT EXISTS `pesanan` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pelanggan_id` int(11) NOT NULL,
+  `nama_produk` varchar(225) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `total_harga` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `pesanan`
+--
+
+INSERT INTO `pesanan` (`id`, `pelanggan_id`, `nama_produk`, `harga`, `qty`, `total_harga`) VALUES
+(1, 1, 'Spidol', 4000, 2, 8000),
+(2, 1, 'Pencil', 2000, 4, 8000),
+(3, 3, 'Buku', 5000, 2, 10000),
+(4, 3, 'Tas', 10000, 1, 10000),
+(5, 14, 'Sepatu', 20000, 3, 60000),
+(6, 14, 'Jam Tangan', 15000, 1, 15000),
+(7, 14, 'Buku', 3000, 5, 15000);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
